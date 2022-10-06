57a8967b574158404bf87a8146299316819013eb
Sebastian Romero <sebastian.romero@imt-atlantique.net>
2022-10-07 00:11:08 +0200

    tutorial

--- movie/movie.py
+++ movie/movie.py
@@ -1,6 +1,6 @@
 from ariadne import graphql_sync, make_executable_schema, load_schema_from_path, ObjectType, QueryType, MutationType
 from ariadne.constants import PLAYGROUND_HTML
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 
 import resolvers as r
 
@@ -9,7 +9,19 @@ HOST = '0.0.0.0'
 app = Flask(__name__)
 
 ### ADD THINGS HERE
-#
+type_defs = load_schema_from_path('movie.graphql')
+query = QueryType()
+movie = ObjectType('Movie')
+query.set_field('movie_with_id', r.movie_with_id)
+
+mutation = MutationType()
+mutation.set_field('update_movie_rate', r.update_movie_rate)
+
+actor = ObjectType('Actor')
+movie.set_field('actors', r.resolve_actors_in_movie)
+query.set_field('actor_with_id', r.actor_with_id)
+
+schema = make_executable_schema(type_defs, movie, query, mutation, actor)
 ###
 
 # root message
@@ -28,11 +40,11 @@ def playground():
 def graphql_server():
     data = request.get_json()
     success, result = graphql_sync(
-    schema,
-    data,
-    context_value=None,
-    debug=app.debug
-    )
+                        schema,
+                        data,
+                        context_value=None,
+                        debug=app.debug
+                    )
     status_code = 200 if success else 400
     return jsonify(result), status_code
 
